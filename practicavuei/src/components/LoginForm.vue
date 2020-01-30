63c140cd2b4e885d73ea850d50b2173493b9d77c
Fernando Gallegos <raccoonevef@gmail.com>
2020-01-30 13:48:55 -0600

    Añadido de la pagina Login y Login form

---
+++ practicavuei/src/components/LoginForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <h1>Formulario para aceder</h1>
+    </section>
+</template>
+
+<script lamg="js">
+export default {
+  name: 'LoginForm'
+}
+</script>
+
+<script lang="scss">
+
+</script>
---
+++ practicavuei/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1> BIENVENIDOS </h1>
+    <LoginForm></LoginForm>
+  </div>
+</template>
+
+<script lang="js">
+import LoginForm from '@/components/LoginForm'
+
+export default {
+  name: 'login',
+  components: {
+    LoginForm
+  }
+}
+</script>
