6fb37640699ec2f32adeb4c4e70ae7e35565e253
DR: SPECTRE <raccoonevef@gmail.com>
2020-02-24 18:37:12 -0700

    [CHG]logInAdd terminada las pruebas

--- practicavuei/src/config/auth.js
+++ practicavuei/src/config/auth.js
@@ -2,47 +2,47 @@ import fireApp from './_firebase'
 import router from '@/router'
 
 export default {
-	signUp(data) {
-		if (data.nombre === '' || data.email === '' || data.password === '') {
-			return alert('Todos los campos son obligatorios')
-		}
-		fireApp.auth().createUserWithEmailAndPassword(data.email, data.password)
-			.then((result) => {
-				let newUser = {
-					displayName: data.nombre,
-					email: result.user.email,
-					uid: result.user.id
-				}
+  signUp (data) {
+    if (data.nombre === '' || data.email === '' || data.password === '') {
+      return alert('Todos los campos son obligatorios')
+    }
+    fireApp.auth().createUserWithEmailAndPassword(data.email, data.password)
+      .then((result) => {
+        let newUser = {
+          displayName: data.nombre,
+          email: result.user.email,
+          uid: result.user.id
+        }
 
-				fireApp.auth().updateCurrentUser(newUser)
-					.catch(err => console.table(err))
-			})
-			.catch((err) => console.table(err))
-		console.log(data)
-	},
+        fireApp.auth().updateCurrentUser(newUser)
+          .catch(err => console.table(err))
+      })
+      .catch((err) => console.table(err))
+    console.log(data)
+  },
 
-	login(data) {
-		fireApp.auth().signInWithEmailAndPassword(data.email, data.password)
-			.then((result) => {
-				console.log(result)
-				router.push('about')
-			})
-			.catch((err) => {
-				console.log(err)
-			})
-	},
-	checkUser() {
-		let user = fireApp.auth().currentUser
+  login (data) {
+    fireApp.auth().signInWithEmailAndPassword(data.email, data.password)
+      .then((result) => {
+        console.log(result)
+        router.push('about')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  },
+  checkUser () {
+    let user = fireApp.auth().currentUser
 
-		if (user) {
-			return user
-		}
+    if (user) {
+      return user
+    }
 
-		return null
-	},
+    return null
+  },
 
-	logOuth() {
-		fireApp.auth().signOut()
-		router.push({ name: 'login'})
-	}
+  logOuth () {
+    fireApp.auth().signOut()
+    router.push({ name: 'login' })
+  }
 }
--- practicavuei/src/views/About.vue
+++ practicavuei/src/views/About.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an {{%route.name}} page</h1>
+    <h1>This is an {{$route.name}} page</h1>
     <input type="button" value="Cerrrar Sessión" @click="logOut" class="btn btn-danger" />
   </div>
 </template>
-
 
 <script lang="js">
 import Auth from '@/config/auth.js'
@@ -12,7 +11,7 @@ export default {
   name: 'About',
 
   methods: {
-    logOut() {
+    logOut () {
       return Auth.logOut()
     }
   }
