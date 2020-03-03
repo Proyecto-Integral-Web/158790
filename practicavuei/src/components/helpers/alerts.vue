16a8fc9a82a895bfb42059453400db38830d5e82
Fernando Gallegos <raccoonevef@gmail.com>
2020-03-03 13:56:32 -0600

    [ADD]master añade errores personalizados en alerts.vue

--- practicavuei/src/components/helpers/alerts.vue
+++ practicavuei/src/components/helpers/alerts.vue
@@ -9,9 +9,22 @@
 <script lang="js">
 export default {
   name: 'AlertComponent',
-  props: ['error', 'message']
+  props: ['error', 'message', 'code'],
+  data () {
+    return {
+      tipo: 'alert-success'
+    }
+  },
+  mounted () {
+    switch (this.code) {
+      case 'auth/user-not-found':
+        this.tipo = 'alert-danger'
+        break
+      default:
+        break
+    }
+  }
 }
 </script>
-
 <style lang="scss">
 </style>
